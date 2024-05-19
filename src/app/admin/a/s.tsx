319be63a1b201bfe6aa8818c69328a29084858a0
SyafiqMSI <muhammadsyafiqibra@gmail.com>
2024-05-19 22:32:31 +0700

    shadcn ui

---
+++ src/app/admin/a/s.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Dashboard } from "../dashbord-admin/page";
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div>
+       <Dashboard />   
+    </div>
+  );
+}
